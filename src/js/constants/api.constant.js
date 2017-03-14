3e5076f8db8e5d1d0d42588bf8d123e312691c21
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-03-14 14:21:44 +0000

    Updating with a gulp-replace

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,7 +1,3 @@
 angular
   .module('myApp')
   .constant('API', 'http://localhost:3000');
-
-  // https://chris-perry-wdi-project-4-api.herokuapp.com (use before pushing to Heroku)
-
-  // http://localhost:3000 (Use when running locally)
